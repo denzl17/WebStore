2dfb6dd35fda607f6a13ed5ce8b2b7bd6f99a421
Denis <56911533+denzl17@users.noreply.github.com>
2020-06-24 21:15:35 +0700

    Урок8

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Employees;
 using WebStore.Domain.Entities.Identity;
 using WebStore.Infrastructure.Interfaces;
 using WebStore.Infrastructure.Mapping;
@@ -8,15 +10,16 @@ using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
+    //[Route("NewRoute/[controller]/123")]
+    //[Route("Staff")]
+    [Authorize]
     public class EmployeesController : Controller
     {
         private readonly IEmployeesData _EmployeesData;
  
-        public EmployeesController(IEmployeesData EmployeesData)
-        {
-            _EmployeesData = EmployeesData;
-        }
+        public EmployeesController(IEmployeesData EmployeesData) => _EmployeesData = EmployeesData;
 
+        //[Route("List")]
         public IActionResult Index() => View(_EmployeesData.Get());
 
         public IActionResult EmployeeDetails(int id)
@@ -31,7 +34,7 @@ namespace WebStore.Controllers
         #region Редактирование
 
         [Authorize(Roles = Role.Administrator)]
-        public IActionResult Edit(int? Id)
+        public IActionResult Edit(int? Id, [FromServices] IMapper Mapper)
         {
             if (Id is null) return View(new EmployeeViewModel());
 
@@ -42,12 +45,13 @@ namespace WebStore.Controllers
             if (employee is null)
                 return NotFound();
 
-            return View(employee.ToView());
+            return View(Mapper.Map<EmployeeViewModel>(employee));
+            //return View(employee.ToView());
         }
 
         [HttpPost]
         [Authorize(Roles = Role.Administrator)]
-        public IActionResult Edit(EmployeeViewModel Model)
+        public IActionResult Edit(EmployeeViewModel Model, [FromServices] IMapper Mapper)
         {
             if (Model is null)
                 throw new ArgumentNullException(nameof(Model));
@@ -64,7 +68,8 @@ namespace WebStore.Controllers
             if (!ModelState.IsValid)
                 return View(Model);
 
-            var employee = Model.FromView();
+            var employee = Mapper.Map<Employee>(Model);
+            //var employee = Model.FromView();
 
             if (Model.Id == 0)
                 _EmployeesData.Add(employee);
