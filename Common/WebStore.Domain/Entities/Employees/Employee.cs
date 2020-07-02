ca63f67742a94250f5778064fbe572870b39498c
Denis <56911533+denzl17@users.noreply.github.com>
2020-07-02 17:35:35 +0700

    lesson2-3

--- Common/WebStore.Domain/Entities/Employees/Employee.cs
+++ Common/WebStore.Domain/Entities/Employees/Employee.cs
@@ -4,22 +4,30 @@ using WebStore.Domain.Entities.Base;
 
 namespace WebStore.Domain.Entities.Employees
 {
+    /// <summary>Информация о сотруднике</summary>
     public class Employee : BaseEntity
     {
+        /// <summary>Имя</summary>
         [Required]
         public string FirstName { get; set; }
 
+        /// <summary>Фамилия</summary>
         [Required]
         public string Surname { get; set; }
 
+        /// <summary>Отчество</summary>
         public string Patronymic { get; set; }
 
+        /// <summary>Возраст</summary>
         public int Age { get; set; }
 
+        /// <summary>Адрес офиса</summary>
         public string Office { get; set; }
 
+        /// <summary>День рожения</summary>
         public DateTime Birthdate { get; set; }
 
+        /// <summary>Хобби</summary>
         public string Hobby { get; set; }
     }
 }
